cdd7462bfcc970bdc2401a924fee31a2532eff6e
clariti23 <g.ferowich@gmail.com>
2020-11-04 19:03:58 -0600

    lesson 1 ch5

--- Contract.sol
+++ Contract.sol
@@ -2,7 +2,15 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
+    // uints must be positive
     uint dnaDigits = 16;
+
+    // use ** for exponentiation
     uint dnaModulus = 10 ** dnaDigits;
 
+    //introduce data type: struct
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
